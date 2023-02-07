e5e37191ce6cfe50899f95a3371fb29331f6af1a
InnocentEdem <80456476+Quessi@users.noreply.github.com>
2023-02-07 20:00:36 +0000

    Add middleware to Application
    Add logger and basicAuth middleware to application for better formatting
    and basic Api Security.

--- server.go
+++ server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"gin-project/controller"
+	"gin-project/middlewares"
 	"gin-project/service"
-
+	"io"
+	"os"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,15 +13,24 @@ var(
 	videoService service.VideoService=service.New()
 	videoController controller.VideoController=controller.New(videoService)
 )
+func setUpLogOutput(){
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
 
 func main() {
-	server := gin.Default()
+	// server := gin.Default()
 
 	// server.GET("/test", func(ctx *gin.Context){
 	// 	ctx.JSON(200, gin.H{
 	// 		"message":"OK!!",
 	// 	})
 	// })
+	setUpLogOutput()
+	server := gin.New()
+	server.Use(gin.Recovery(),middlewares.Logger(),middlewares.BasicAuth(),)
+
+
 	server.GET("/videos", func(ctx *gin.Context){
 		ctx.JSON(200, videoController.FindAll())
 	})
